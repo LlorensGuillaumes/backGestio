304589dcc5433039b96563f9f952847432183f28
JoanLlorens <j.llorens@uniogestio.es>
2026-07-07 16:51:37 +0200

    Add SQL script to register gestio_deb in master database

---
+++ sql/agregar_gestio_deb.sql
@@ -0,0 +1,12 @@
+-- Agregar gestio_deb a la tabla bases_datos
+-- Ejecutar conectado a la base de datos master
+
+INSERT INTO "bases_datos" ("nombre", "db_name", "db_host", "db_port", "activa") VALUES
+('Gestio Deb', 'gestio_deb', 'ep-noisy-sound-asv7n572-pooler.c-4.eu-central-1.aws.neon.tech', 5432, true)
+ON CONFLICT ("db_name") DO UPDATE SET
+  db_host = 'ep-noisy-sound-asv7n572-pooler.c-4.eu-central-1.aws.neon.tech',
+  db_port = 5432,
+  activa = true;
+
+-- Verificar que se agregó correctamente
+SELECT * FROM bases_datos WHERE db_name = 'gestio_deb';
